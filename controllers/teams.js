bca8381d78790651ced430d8e1d5aae7f274c195
Max Sauber <maxsauber@Maxs-Air.fios-router.home>
2020-04-16 21:59:29 -0400

    getNextId added

--- controllers/teams.js
+++ controllers/teams.js
@@ -14,22 +14,29 @@ const getTeamById = (request, response) => {
     : response.sendStatus(404)
 }
 
+const getNextId = () => {
+  const lastId = teams.reduce((acc, team) => {
+    return team.id > acc ? team.id : acc
+  }, 0)
+
+  return lastId + 1
+} // Watch Explainer Video for a review of the reduce() function....
+
 const saveNewTeam = (request, response) => {
   const {
-    id, location, mascot, abbreviation, conference, division
+    location, mascot, abbreviation, conference, division
   } = request.body
 
-  // console.log(teams.length + 1) // Starting Extra Credit...
-
-  if (!id || !location || !mascot || !abbreviation || !division) {
-    return response.status(400)
+  if (!location || !mascot || !abbreviation || !division) {
+    return response
+      .status(400)
       .send('The following fields are required: id, location, mascot, abbreviation, conference, division')
   }
 
   const newTeam = {
-    id, location, mascot, abbreviation, conference, division
+    id: getNextId(), location, mascot, abbreviation, conference, division
   }
-  // Example Test Data:  { "id": "6000", "location": "Everywhere", "mascot": "A Silly Goat", "abbreviation": "GOAT", "conference": "XYZ", "division": "East" }
+  // Example Test Data:  { "location": "Everywhere", "mascot": "A Silly Goat", "abbreviation": "GOAT", "conference": "XYZ", "division": "East" }
 
   teams.push(newTeam)
 
