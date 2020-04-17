9fefbf14499f27d0e80c9f6f72f6b83e864f5f23
Max Sauber <maxsauber@Maxs-Air.fios-router.home>
2020-04-16 21:21:11 -0400

    part two answer

--- controllers/teams.js
+++ controllers/teams.js
@@ -4,11 +4,36 @@ const getAllTeams = (request, response) => {
   return response.send(teams)
 }
 
-const getTeamsById = (request, response) => {
+const getTeamById = (request, response) => {
   const { id } = request.params
-  const locateTeam = teams.filter(team => team.id === Number(id))
 
-  return response.send(locateTeam)
+  const matchingTeams = teams.find((team) => team.id === parseInt(id))
+
+  return matchingTeams
+    ? response.send(matchingTeams)
+    : response.sendStatus(404)
 }
 
-module.exports = { getAllTeams, getTeamsById }
+const saveNewTeam = (request, response) => {
+  const {
+    id, location, mascot, abbreviation, conference, division
+  } = request.body
+
+  // console.log(teams.length + 1) // Starting Extra Credit...
+
+  if (!id || !location || !mascot || !abbreviation || !division) {
+    return response.status(400)
+      .send('The following fields are required: id, location, mascot, abbreviation, conference, division')
+  }
+
+  const newTeam = {
+    id, location, mascot, abbreviation, conference, division
+  }
+  // Example Test Data:  { "id": "6000", "location": "Everywhere", "mascot": "A Silly Goat", "abbreviation": "GOAT", "conference": "XYZ", "division": "East" }
+
+  teams.push(newTeam)
+
+  return response.status(201).send(newTeam)
+}
+
+module.exports = { getAllTeams, getTeamById, saveNewTeam }
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
-const { getAllTeams, getTeamsById } = require('./controllers/teams')
+const bodyParser = require('body-parser')
+const { getAllTeams, getTeamById, saveNewTeam } = require('./controllers/teams')
 
 const app = express()
 
 app.get('/teams', getAllTeams)
 
-app.get('/teams/:id', getTeamsById)
+app.get('/teams/:id', getTeamById)
+
+app.post('/teams', bodyParser.json(), saveNewTeam)
 
 app.listen(1337, () => {
   console.log('Listening on port 1337...') // eslint-disable-line no-console
